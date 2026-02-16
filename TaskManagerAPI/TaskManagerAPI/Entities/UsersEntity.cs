629492a9d0d324641d7b7b163b6c9d889f87a0dc
DanielAmador18 <danielmedina051216@gmail.com>
2026-02-16 10:35:48 -0700

    Add README

---
+++ TaskManagerAPI/TaskManagerAPI/Entities/UsersEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace TaskManagerAPI.Entities
+{
+    public class UsersEntity
+    {
+        public int Id { get; set; }
+        public string Rol { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
